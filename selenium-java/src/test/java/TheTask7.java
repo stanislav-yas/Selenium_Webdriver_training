aff38abc40087bf1895883cc92b7110947f3831b
Stas <stanislav.yas@gmail.com>
2018-05-25 06:58:54 +0600

    Задание 7 +

--- selenium-java/src/test/java/TheTask7.java
+++ selenium-java/src/test/java/TheTask7.java
@@ -1,9 +1,11 @@
+import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.*;
-import java.util.List;
 import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
 
 public class TheTask7 extends TestBase{
+
+    private int clickCount = 0;
 
     @Test
     public void checkMenu(){
@@ -12,11 +14,25 @@ public class TheTask7 extends TestBase{
         driver.findElement(By.name("password")).sendKeys("admin");
         driver.findElement(By.name("login")).click();
         wait.until(titleIs("My Store"));
-        WebElement menu = driver.findElement(By.cssSelector("#box-apps-menu"));
-        List<WebElement> items = menu.findElements(By.cssSelector("li"));
-        List<WebElement> elements = driver.findElements(By.cssSelector("#box-apps-menu li a"));
-        for(WebElement element:elements){
-            element.click();
+        String cssSelectorUL = "ul#box-apps-menu.list-vertical";      // left menu CSS Selector
+        clickAllItemsWithChildren(cssSelectorUL);
+        System.out.println(clickCount + " time(s) clicked");
+        Assert.assertEquals("Inconsistency in the number of menu items",48,clickCount);
+    }
+
+    private void clickAllItemsWithChildren(String cssSelectorUL){
+        int size = driver.findElements(By.cssSelector(cssSelectorUL + " >li")).size(); // list item count
+        for(int i = 1; i <= size; i++) {
+            String cssSelectorLI = cssSelectorUL + " >li:nth-child(" + i + ")";
+            WebElement li = driver.findElement(By.cssSelector(cssSelectorLI)); // find n-th list item
+            if(li.getAttribute("class").equals("selected")) continue;  // skip already selected item
+            li.click(); clickCount++;
+            System.out.println("h1 = " + driver.findElement(By.tagName("h1")).getText()); // check for 'h1' tag
+            String cssSelectorULChild = cssSelectorLI + " >ul";
+            if(driver.findElements(By.cssSelector(cssSelectorULChild)).size()>0){ // check for submenu existence
+                clickAllItemsWithChildren(cssSelectorULChild); // to click submenu recursively
+            }
         }
     }
+
 }
